ba0c52e36e0dfd9e6e215ae08561e8d3aa86fd48
preetshah02 <preetshah02@gmail.com>
2023-11-23 15:51:04 -0800

    First

---
+++ src/test/java/com/javaguides/driving/DrivingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaguides.driving;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DrivingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
